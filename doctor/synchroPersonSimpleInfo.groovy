e7b8269f20ebd1a1f996c287b156965b1caeea51
mazekkkk <mazekkkk@hotmail.com>
2018-03-23 16:47:53 +0800

    modify by maze 2018-03-23 初始化脚本

---
+++ doctor/synchroPersonSimpleInfo.groovy
@@ -0,0 +1,5 @@
+
+ctx._source.image=image;
+ctx._source.tags=special;
+ctx._source.biography=biography;
+ctx._source.academicAchievements=academicAchievements;
---
+++ hospital/upsert.groovy
@@ -0,0 +1,51 @@
+if (code && "" != code){
+	ctx._source.code=code;
+}
+if (name && "" != name){
+	ctx._source.name=name;
+}
+if (icon && "" != icon){
+	ctx._source.icon=icon;
+}
+if (null!=image && "" != image){
+	ctx._source.image=image;
+}
+if (phone && "" != phone){
+	ctx._source.phone=phone;
+}
+if (summary && "" != summary){
+	ctx._source.summary=summary;
+}
+if (null!=score){
+	ctx._source.score=score;
+}
+if (null!=expertCount){
+	ctx._source.expertCount=expertCount;
+}
+if (address && "" != address){
+	ctx._source.address=address;
+}
+if (null!=isInsurance){
+	ctx._source.isInsurance=isInsurance;
+}
+if (null!=coordinates){
+	ctx._source.coordinates=coordinates;
+}
+if (null!=type){
+	ctx._source.type=type;
+}
+if (null!=level){
+	ctx._source.level=level;
+}
+if (null!=area){
+	ctx._source.area=area;
+}
+if (null!=dept){
+	ctx._source.dept=dept;
+}
+if (null!=createTime){
+	ctx._source.createTime=createTime;
+}
+if (null!=updateTime){
+	ctx._source.updateTime=updateTime;
+}
